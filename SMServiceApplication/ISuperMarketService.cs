ad0e6aefb7b5d720f1d5dcb0feb6872b3781c5b3
Huy Lo <Huy Lo@DESKTOP-D3D0DUB>
2020-11-15 23:46:08 +0700

    service

--- SMServiceApplication/ISuperMarketService.cs
+++ SMServiceApplication/ISuperMarketService.cs
@@ -224,5 +224,155 @@ namespace SMServiceApplication
           UriTemplate = "api/v1/OrderDetail/Delete/{id}"
         )]
         bool DeleteOrderDetail(string id);
+
+        //Payment Services
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Customer/GetList"
+            )]
+        List<Payment> GetPayments();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Payment/Create"
+            )]
+        bool AddPayment(Payment payment);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Payment/Edit/{id}"
+            )]
+        bool EditPayment(string id, Payment payment);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Payment/Delete/{id}"
+            )]
+        bool DeletePayment(string id);
+
+        //Employee Services
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Employee/GetList"
+            )]
+        List<Employee> GetEmployees();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Employee/Create"
+            )]
+        bool AddEmployee(Employee employee);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Employee/Edit/{id}"
+            )]
+        bool EditEmployee(string id, Employee employee);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Employee/Delete/{id}"
+            )]
+        bool DeleteEmployee(string id);
+
+
+        //Role service
+
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Role/GetList"
+            )]
+        List<Role> GetRoles();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Role/Create"
+            )]
+        bool AddRole(Role role);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Role/Edit/{id}"
+            )]
+        bool EditRole(string id, Role Role);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Role/Delete/{id}"
+            )]
+        bool DeleteRole(string id);
+
+        //Provider service 
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Provider/GetList"
+            )]
+        List<Provider> GetProviders();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Provider/Create"
+            )]
+        bool AddProvider(Provider provider);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Provider/Edit/{id}"
+            )]
+        bool EditProvider(string id, Provider provider);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Provider/Delete/{id}"
+            )]
+        bool DeleteProvider(string id);
     }
 }
